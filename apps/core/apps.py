be01fbc66ac4348f0d72bb2fd5ffd2683d933896
kl4us <antonioposta@gmail.com>
2026-04-27 20:04:34 +0200

    add admin site configuration to CoreConfig

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -2,4 +2,12 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
-    name = 'apps.core'
+    name = "apps.core"
+
+    def ready(self):
+        from django.conf import settings
+        from django.contrib import admin
+
+        admin.site.site_header = settings.BRAND_NAME
+        admin.site.site_title = settings.PROJECT_TITLE
+        admin.site.index_title = f"Welcome to {settings.BRAND_NAME}"
